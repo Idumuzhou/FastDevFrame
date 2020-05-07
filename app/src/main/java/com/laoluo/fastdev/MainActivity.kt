62c6cdf22d99362c0ae68a91eddf16267ae460ac
xuliang.luo <962526394@qq.com>
2020-05-07 09:27:47 +0800

    1.update

--- app/src/main/java/com/laoluo/fastdev/MainActivity.kt
+++ app/src/main/java/com/laoluo/fastdev/MainActivity.kt
@@ -2,6 +2,7 @@ package com.laoluo.fastdev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.laoluo.fastdev.aretrofit.ARetrofit
 
 class MainActivity : AppCompatActivity() {
 
